6c6efc38ea9dcc692ed587b7fa83308cde6791ea
Rafael Gonzalez <astrorafael@gmail.com>
2024-07-11 10:52:48 +0200

    updated Rome enum

--- src/lica/asyncio/photometer/__init__.py
+++ src/lica/asyncio/photometer/__init__.py
@@ -30,6 +30,9 @@ class Role(enum.IntEnum):
     def __str__(self):
         return f"{self.name.lower()}"
 
+    def __repr__(self):
+        return f"{self.name.upper()}"
+
     def __iter__(self):
         return self
 
