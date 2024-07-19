1752cc42fe49fc4d26e754b3421709d4c42f0185
Rafael Gonzalez <astrorafael@gmail.com>
2024-07-19 10:12:07 +0200

    adding validators

--- src/lica/validators.py
+++ src/lica/validators.py
@@ -19,19 +19,19 @@ import functools
 # Module utility functions
 # ------------------------
 
-def vfile(path):
+def vfile(path: str) -> str:
     """File validator for the command line interface"""
     if not os.path.isfile(path):
         raise IOError(f"Not valid or existing file: {path}")
     return path
 
-def vdir(path):
+def vdir(path: str) -> str:
     """Directory validator for the command line interface"""
     if not os.path.isdir(path):
         raise IOError(f"Not valid or existing directory: {path}")
     return path
 
-def vbool(boolstr):
+def vbool(boolstr: str) -> bool:
     """Boolean text validator for the command line interface"""
     result = None
     if boolstr == 'True':
@@ -40,7 +40,7 @@ def vbool(boolstr):
         result = False
     return result
 
-def vdate(datestr):
+def vdate(datestr: str) -> datetime.datetime:
     """Date & time validator for the command line interface"""
     date = None
     for fmt in ['%Y-%m', '%Y-%m-%d', '%Y-%m-%dT%H:%M:%S', '%Y-%m-%dT%H:%M:%SZ']:
@@ -50,28 +50,31 @@ def vdate(datestr):
             pass
     return date
 
-def vmonth(datestr):
+def vmonth(datestr: str) -> datetime.datetime:
     return datetime.datetime.strptime(datestr, '%Y-%m')
 
-def vfloat(num):
+def vyear(datestr: str) -> datetime.datetime:
+    return datetime.datetime.strptime(datestr, '%Y')
+
+def vfloat(num: str) -> float:
     '''Validator that admits fractions'''
     return float(fractions.Fraction(num))
 
-def vfloat01(num):
+def vfloat01(num: str) -> float:
     '''Validator between [0..1] that admits fractions'''
     num = float(fractions.Fraction(num))
     if not (0.0 <= num <= 1.0):
         raise ValueError(f"Value {num} out of bounds [0..1]")
     return num
 
-def vint(L, H, num):
+def vint(L: int, H: int, num: str) -> int:
     '''Integer validator between [L..H]'''
     num = int(num)
     if not (L <= num <= H):
         raise ValueError(f"Value {num} out of bounds [{L}..{H}]")
     return num
 
-def voddint(L, H, num):
+def voddint(L: int, H: int, num: str) -> int:
     '''Odd integer validator between [L..H]'''
     num = int(num)
     if (L % 2) == 0:
@@ -84,7 +87,7 @@ def voddint(L, H, num):
         raise ValueError(f"Value {num} out of bounds [{L}..{H}]")
     return num
 
-def vevenint(L, H, num):
+def vevenint(L: int, H: int, num: str) -> int:
     '''Even integer validator between [L..H]'''
     num = int(num)
     if (L % 2) == 1:
@@ -97,7 +100,7 @@ def vevenint(L, H, num):
         raise ValueError(f"Value {num} out of bounds [{L}..{H}]")
     return num
 
-def vflopath(value):
+def vflopath(value: [float|str]) -> float:
     '''Validator that admits either a single number or a file (representing an image)'''
     try:
         n = float(fractions.Fraction(value))
@@ -107,7 +110,7 @@ def vflopath(value):
         return value
     return n
 
-def vmac(mac):
+def vmac(mac: str) -> str:
     ''''Valid input MAC strings'''
     try:
         corrected_mac = ':'.join(f"{int(x,16):02X}" for x in mac.split(':'))
