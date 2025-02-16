a8ebbd1f9789b4b3c239a47cad2367639c6a1fd0
astrorafael <astrorafael@gmail.com>
2025-02-16 09:50:05 +0100

    no aiosqlite logging for the time being

--- src/lica/sqlalchemy/__init__.py
+++ src/lica/sqlalchemy/__init__.py
@@ -5,6 +5,5 @@ from argparse import Namespace
 def sqa_logging(args: Namespace) -> None:
     if args.verbose:
         logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
-        logging.getLogger("aiosqlite").setLevel(logging.INFO)
     else:
         logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
