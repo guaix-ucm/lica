2f33c63e8a840fea1328c5889e3d5e313c8a638d
astrorafael <astrorafael@gmail.com>
2025-02-16 09:52:04 +0100

    went back to suppress aiosqlite messages

--- src/lica/sqlalchemy/__init__.py
+++ src/lica/sqlalchemy/__init__.py
@@ -5,5 +5,7 @@ from argparse import Namespace
 def sqa_logging(args: Namespace) -> None:
     if args.verbose:
         logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+        # This is needed, otherwise aiosqlite msgs will jump in
+        logging.getLogger("aiosqlite").setLevel(logging.INFO)
     else:
         logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
