908d18e3efb76df0d4837a16fa1fe8ee09532e0a
Rafael Gonzalez <astrorafael@gmail.com>
2025-02-17 13:42:35 +0100

    added table format size default argument

--- src/lica/tabulate.py
+++ src/lica/tabulate.py
@@ -14,11 +14,11 @@ import tabulate
 from .misc import group
 
 
-def paging(cursor, headers, page_size=10):
+def paging(iterable, headers, page_size=10, table_fmt="simple"):
     """
     Pages query output and displays in tabular format
     """
-    for rows in group(page_size, cursor):
-        print(tabulate.tabulate(rows, headers=headers, tablefmt="simple"))
+    for rows in group(page_size, iterable):
+        print(tabulate.tabulate(rows, headers=headers, tablefmt=table_fmt))
         if len(rows) == page_size:
             input("Press Enter to continue [Ctrl-C to abort] ...")
