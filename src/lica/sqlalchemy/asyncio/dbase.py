3f54bdb9aa202c7654bd2ed4f303e16b23b7ead6
Rafael González <astrorafael@gmail.com>
2025-08-30 08:10:16 +0200

    added logging_name tag in case of multiple engines

--- src/lica/sqlalchemy/asyncio/dbase.py
+++ src/lica/sqlalchemy/asyncio/dbase.py
@@ -18,10 +18,13 @@ import decouple
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-def create_engine_sessionclass(env_var: str = "DATABASE_URL") -> Tuple[Engine,Any]:
+def create_engine_sessionclass(env_var: str = "DATABASE_URL", tag: str = None) -> Tuple[Engine,Any]:
 	url = decouple.config(env_var)
-	# 'check_same_thread' is only needed in SQLite ....
-	engine = create_async_engine(url, connect_args={"check_same_thread": False})
+	if tag:
+		# 'check_same_thread' is only needed in SQLite ....
+		engine = create_async_engine(url, logging_name=tag, connect_args={"check_same_thread": False})
+	else:
+		engine = create_async_engine(url, connect_args={"check_same_thread": False})
 	Session = async_sessionmaker(engine, expire_on_commit=False)
 	return engine, Session
 
